7c0d253462e277d45c90f78f8cf78a591351e878
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-01-02 23:35:09 +0100

    added train stops for metro train, might work, might not work, idk
    its somewhat broken but somewhat works at the same time, just don't restart the resource while in-game and no errors should occur.

--- TrainsClient/Main.cs
+++ TrainsClient/Main.cs
@@ -26,8 +26,53 @@ namespace TrainsClient
             "FREIGHTCONT1",
             "FREIGHTCONT2",
             "FREIGHTGRAIN",
+            "TANKERCAR",
             "METROTRAIN",
-            "TANKERCAR"
+            "S_M_M_LSMETRO_01" // not a train, but this model should also be loaded. (train driver ped model)
+        };
+
+        // Metro train station stops
+        private readonly List<Vector3> TrainStops = new List<Vector3>()
+        {
+            // los santos airport (airport front door entrance)
+            new Vector3(-1088.627f, -2709.362f, -7.137819f),
+            new Vector3(-1081.309f, -2725.259f, -7.137033f),
+
+            // los santos airport (car park/highway entrance)
+            new Vector3(-889.2755f, -2311.825f, -11.45941f),
+            new Vector3(-876.7512f, -2323.808f, -11.45609f),
+
+            // Little Seoul (near los santos harbor)
+            new Vector3(-545.3138f, -1280.548f, 27.09238f),
+            new Vector3(-536.8082f, -1286.096f, 27.08768f),
+
+            // Strawberry (near strip club)
+            new Vector3(270.2029f, -1210.818f, 39.25398f),
+            new Vector3(265.3616f, -1198.051f, 39.23406f), // vector3(262.6042, -1210.32, 39.23338)
+
+            // Rockford Hills (San Vitus Blvd)
+            new Vector3(-286.3837f, -318.8773f, 10.33625f),
+            new Vector3(-302.6719f, -322.9958f, 10.33629f),
+
+            // Rockford Hills (Near golf club)
+            new Vector3(-826.3845f, -134.7151f, 20.22362f),
+            new Vector3(-816.7159f, -147.4567f, 20.2231f),
+
+            // Del Perro (Near beach)
+            new Vector3(-1351.282f, -481.2916f, 15.318f),
+            new Vector3(-1341.085f, -467.674f, 15.31838f),
+
+            // Little Seoul
+            new Vector3(-496.0209f, -681.0325f, 12.08264f),
+            new Vector3(-495.8456f, -665.4668f, 12.08244f),
+
+            // Pillbox Hill (Downtown)
+            new Vector3(-218.2868f, -1031.54f, 30.51112f),
+            new Vector3(-209.6845f, -1037.544f, 30.50939f),
+
+            // Davis (Gang / hood area)
+            new Vector3(112.3714f, -1729.233f, 30.24097f),
+            new Vector3(120.0308f, -1723.956f, 30.31433f)
         };
 
         /// <summary>
@@ -36,8 +81,68 @@ namespace TrainsClient
         public Main()
         {
             //EventHandlers.Add("playerSpawned", new Action(Spawn));
-            Spawn();
-            EventHandlers.Add(GetCurrentResourceName() + ":GetTrainNetworkHandle", new Action<List<dynamic>>(SetTrain));
+            //Spawn();
+            //EventHandlers.Add(GetCurrentResourceName() + ":GetTrainNetworkHandle", new Action<List<dynamic>>(SetTrain));
+            Tick += ManageTrainStops;
+        }
+
+        List<int> stoppingTrains = new List<int>();
+
+        private async Task ManageTrainStops()
+        {
+            //SetRandomTrains(true);
+            foreach (var train in TrainHandles)
+            {
+                if (DoesEntityExist(train))
+                {
+                    if (IsVehicleModel(train, (uint)GetHashKey("METROTRAIN")))
+                    {
+                        if (GetEntitySpeed(train) < 1f)
+                        {
+                            for (var i = 0; i < GetNumberOfVehicleDoors(train); i++)
+                            {
+                                SetVehicleDoorOpen(train, i, false, false);
+                                SetVehicleDoorOpen(GetTrainCarriage(train, 1), i, false, false);
+                            }
+                        }
+                        else
+                        {
+                            for (var i = 0; i < GetNumberOfVehicleDoors(train); i++)
+                            {
+                                SetVehicleDoorShut(train, i, false);
+                                SetVehicleDoorShut(GetTrainCarriage(train, 1), i, false);
+                            }
+                        }
+
+
+                        Vector3 pos = GetEntityCoords(train, false);
+                        if ((stoppingTrains.Count > 0 && !stoppingTrains.Contains(train)) || stoppingTrains.Count < 1)
+                        {
+                            if (TrainStops.Any((stop) => stop.DistanceToSquared(pos) < 10f))
+                            {
+                                StopTrain(train);
+                            }
+                        }
+                    }
+                }
+            }
+            await Task.FromResult(0);
+        }
+
+        private async void StopTrain(int train)
+        {
+            stoppingTrains.Add(train);
+            SetTrainCruiseSpeed(train, 0f);
+
+            await Delay(20000);
+
+            SetTrainCruiseSpeed(train, 8f);
+            //while (GetEntitySpeed(train) < 6f)
+            //{
+            //    await Delay(100);
+            //}
+            await Delay(5000);
+            stoppingTrains.Remove(train);
         }
 
         /// <summary>
@@ -140,6 +245,8 @@ namespace TrainsClient
                 }
 
                 //speedv
+                SetRandomTrains(false);
+                DeleteAllTrains();
                 if (NetworkIsHost())
                 {
                     bool direction = new Random().Next(0, 1) == 1;
@@ -149,41 +256,49 @@ namespace TrainsClient
                         new Vector3(3007f, 4105f, 54f),
                         new Vector3(1010f, 3218f, 40f),
                         new Vector3(665f, -729f, 24f),
-                        new Vector3(-594f, -1391f, 21f),
+                        //new Vector3(-594f, -1391f, 21f),
+                        new Vector3(40.2f, -1201.3f, 31.0f),
                     };
 
                     for (var i = 0; i < 4; i++)
                     {
-                        if (!DoesEntityExist(TrainHandles[i]))
+                        if ((TrainHandles.Count > i && !DoesEntityExist(TrainHandles[i])) || (TrainHandles.Count <= i))
                         {
                             if (i == 3)
                             {
-                                TrainHandles[i] = CreateMissionTrain(24, coords[i].X, coords[i].Y, coords[i].Z, direction);
+                                TrainHandles[i] = CreateMissionTrain(24, coords[i].X, coords[i].Y, coords[i].Z, true);
                             }
                             else
                             {
                                 TrainHandles[i] = CreateMissionTrain(new Random().Next(2, 15), coords[i].X, coords[i].Y, coords[i].Z, direction);
                             }
-
+                            Debug.WriteLine($"Train {i} id: {TrainHandles[i]}");
                         }
+
+                        //N_0x2b6747faa9db9d6b(TrainHandles[i], true);
+                        var ped = GetPedInVehicleSeat(TrainHandles[i], -1);
+                        if (DoesEntityExist(ped))
+                        {
+                            DeletePed(ref ped);
+                        }
+                        var newped = CreatePedInsideVehicle(TrainHandles[i], 26, (uint)GetHashKey("s_m_m_lsmetro_01"), -1, true, true);
+                        //if (IsVehicleModel(TrainHandles[i], (uint)GetHashKey("METROTRAIN")))
+                        //{
+                        //    TaskVehicleDriveWander(newped, TrainHandles[i], 8f, 786603);
+                        //}
+                        //SetVehicleSt(TrainHandles[i], true);
+                        SetVehicleFixed(TrainHandles[i]);
+                        //CitizenFX.Core.Native.Function.Call((CitizenFX.Core.Native.Hash)0xE861D0B05C7662B8, newped, 1, 1);
+                        SetPedDefaultComponentVariation(newped);
+                        ClearPedDecorations(newped);
+
+
+
+
+                        //N_0xe861d0b05c7662b8(newped, 0, 0);
+                        //}
                     }
 
-                    //if (!DoesEntityExist(TrainHandles[0]))
-                    //{
-                    //    TrainHandles[0] = CreateMissionTrain(new Random().Next(2, 15), 3007, 4105, 54, direction);
-                    //}
-                    //if (!DoesEntityExist(TrainHandles[1]))
-                    //{
-                    //    TrainHandles[1] = CreateMissionTrain(new Random().Next(2, 15), 1010f, 3218f, 40f, direction);
-                    //}
-                    //if (!DoesEntityExist(TrainHandles[2]))
-                    //{
-                    //    TrainHandles[2] = CreateMissionTrain(new Random().Next(2, 15), 665f, -729f, 24f, direction);
-                    //}
-                    //for (var ii = 0; ii < TrainHandles.Length; ii++)
-                    //{
-                    //    TrainNetHandles[ii] = VehToNet(TrainHandles[ii]);
-                    //}
                     List<int> trainNetHandles = new List<int>();
                     foreach (int tHandle in TrainHandles)
                     {
